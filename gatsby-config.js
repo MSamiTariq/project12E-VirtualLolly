39f8e3d9df764e8bc3deb9d19aefb51af8052ce8
samitariq12 <samitariq40@gmail.com>
2020-12-29 20:34:50 +0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,18 @@ module.exports = {
       options: {
         uri: 'https://example.com/graphql'
       }
-    }
-    // {
-    //   resolve: "gatsby-source-graphql",
-    //   options: {
-    //     // Arbitrary name for the remote schema Query type
-    //     typeName: "Lolly",
-    //     // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-    //     fieldName: "LOLLIES",
-    //     // Url to query from
-    //     url: "https://virtual-lolly-2020.netlify.app/.netlify/functions/lollyFunction",
-    //   },
-    // },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "Lolly",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "LOLLIES",
+        // Url to query from
+        url: "https://virtual-lolly-2020.netlify.app/.netlify/functions/lollyFunction",
+      },
+    },
 
   ],
 }
---
+++ gatsby-node.js
@@ -0,0 +1,31 @@
+const path = require('path');
+exports.createPages = async ({actions, graphql}) => {
+  console.log(graphql);
+    const result = await graphql(
+      `{
+            LOLLIES{
+                getAllLollies{
+                recipientName
+                sender
+                message
+                flavourTop
+                flavourMid
+                flavourBottom
+                lollyPath
+                }
+              
+          }
+      }
+    `)
+    console.log("from node.js", result)
+
+    // result.data.getAllLollies.map(async (indLolly) => {
+    //     await actions.createPage({
+    //         path: `lolly/${indLolly.path}`,
+    //         component: path.resolve(`./src/template/lollyPage.js`),
+    //         context: {
+    //             lolly: indLolly
+    //         }
+    //     })
+    // })
+}

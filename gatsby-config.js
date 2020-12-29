ce16ef76a3ed96f2f97ab6d2b5b15f86537e2609
samitariq12 <samitariq40@gmail.com>
2020-12-29 23:01:46 +0500

    gatsby-config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: "gatsby-source-graphql",
       options: {
         // Arbitrary name for the remote schema Query type
-        typeName: "Lolly",
+        typeName: "GCMS",
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: "LOLLIES",
         // Url to query from
